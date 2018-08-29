878813d2f7f8a14475926e3a6c48a97b9b677ff6
Maciej Satkiewicz <msatkiewicz@gmail.com>
2018-08-29 11:16:23 +0200

    cd łysy -> bald

--- lib/local_method_callbacks/error.rb
+++ lib/local_method_callbacks/error.rb
@@ -1,6 +1,6 @@
 module LocalMethodCallbacks
   # http://blog.honeybadger.io/ruby-exception-vs-standarderror-whats-the-difference/
-  # bold "rescue" catches StandardError, but not Exception
+  # bald "rescue" catches StandardError, but not Exception
   class Error < StandardError
   end
 
